a964ed9907bfb7c8facdb76d16e628629615142f
Leonardo Cox <leonardocx0@gmail.com>
2022-07-15 10:52:29 -0400

    My first commit woo! and added 404 page route

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// Require needed modules & initialize the app object
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
+// Create a homepage routine
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page</h1>");
+});
+
+// Listen for connections
+app.listen(process.env.PORT, function () {
+  console.log("I am awake!");
+});
