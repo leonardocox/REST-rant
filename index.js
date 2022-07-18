3df3a09a80aac97bd5b775fa1042e1e90e48f3c2
Leonardo Cox <leonardocx0@gmail.com>
2022-07-18 11:59:35 -0400

    Added home page view and router

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// Home page router
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
 // Import router
 app.use("/places", require("./controllers/places"));
 
 // Create a homepage routine
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.render("home");
 });
 
 // 404 page route
