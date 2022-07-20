e6a1d4d0c5466037b9d92a65dbdc1f368b740c04
Leonardo Cox <leonardocx0@gmail.com>
2022-07-20 10:38:54 -0400

    Form data is going through now

--- controllers/places.js
+++ controllers/places.js
@@ -10,4 +10,9 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("POST /places");
+});
+
 module.exports = router;
