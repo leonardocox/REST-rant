73e9bff1d9112f44dc04b585022bca8da63be126
Leonardo Cox <leonardocx0@gmail.com>
2022-07-22 13:35:49 -0400

    Full delete button funtionality

--- controllers/places.js
+++ controllers/places.js
@@ -21,6 +21,18 @@ router.get("/:id", (req, res) => {
   }
 });
 
+router.delete("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    places.splice(id, 1);
+    res.redirect("/places");
+  }
+});
+
 router.post("/", (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
